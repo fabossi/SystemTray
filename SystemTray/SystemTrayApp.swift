7af1df767a1a517d48eec49e721d4e470c8559df
Felipe Fabossi <45209862+fabossi@users.noreply.github.com>
2025-03-18 14:16:23 -0300

    Initial Commit

---
+++ SystemTray/SystemTrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemTrayApp.swift
+//  SystemTray
+//
+//  Created by Felipe Fabossi on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SystemTrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
